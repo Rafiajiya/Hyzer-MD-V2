079ec0d2ab1f397068ff17b0aeaaf79d60b5be0c
Rafiajiya <94803405+Rafiajiya@users.noreply.github.com>
2022-04-07 13:21:55 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -5,19 +5,20 @@ let handler = async (m) => {
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
     let user = global.db.data.users[who]
-let anu = `*─────� DONATE 」 ───*
+let anu = `*â”€â”€â”€â”€â”€Œ DONATE ã€ â”€â”€â”€*
 
-Hai 👋
+Hai ðŸ‘‹
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
-┌〔 Donasi • Emoney 〕
-├📌Indoosat: 085643354073
-└────
-Berapapun donasi kalian akan sangat berarti 👍
+â”Œã€” Donasi â€¢ Emoney ã€•
+â”œðŸ“ŒSmartfren: 088233481992
+â””â”€â”€â”€â”€
+Berapapun donasi kalian akan sangat berarti ðŸ‘
 
-Arigatou!
+Gue tau kok Bro lu Cmn pencet doang :)
+_-
 
 Contact person Owner:
-wa.me/6285643354073 (Owner)
+wa.me/6288233481992 (Owner)
 
 *donasi via follow ig juga boleh*`
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
@@ -29,7 +30,7 @@ wa.me/6285643354073 (Owner)
            hydratedFooterText: wm,
            hydratedButtons: [{
              urlButton: {
-               displayText: '📍instagram',
+               displayText: 'ðŸ“instagram',
                url: instagram
              }
 
